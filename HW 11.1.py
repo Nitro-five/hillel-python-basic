e7e5a10c9cf5aef7e1d5124b838eefd1d3a5dcac
Max <avtobtos1235@gmail.com>
2024-08-04 16:39:45 +0300

    HW 11

---
+++ HW 11.1.py
@@ -0,0 +1,29 @@
+def prime_generator(end):
+    def func_prime(n):
+        if n <= 1:
+            return False
+        if n <= 3:
+            return True
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        i = 5
+        while i * i <= n:
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+
+    number = 2
+    while number <= end:
+        if func_prime(number):
+            yield number
+        number += 1
+
+from inspect import isgenerator
+
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
